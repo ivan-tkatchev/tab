19cf491d5a150f3b691c7220c5dd93c31de0f76c
Ivan Tkachev <ivan.tkachev@annalect.omd.ru>
2019-04-26 13:25:22 +0300

    Added the "product", "mul" and "add" functions.

--- funcs/sum.h
+++ funcs/sum.h
@@ -1,44 +1,52 @@
 #ifndef __TUP_FUNCS_SUM_H
 #define __TUP_FUNCS_SUM_H
 
-template <typename T>
-struct AtomSum : public obj::Atom<T> {
+template <bool MUL, typename T>
+struct AtomSumMul : public obj::Atom<T> {
 
     obj::Object* clone() const {
-        AtomSum<T>* ret = new AtomSum<T>;
+        AtomSumMul<MUL, T>* ret = new AtomSumMul<MUL, T>;
         ret->v = this->v;
         return ret;
     }
     
     void merge(const obj::Object* o) {
-        this->v += obj::get< obj::Atom<T> >(o).v;
+	if (MUL) {
+	    this->v *= obj::get< obj::Atom<T> >(o).v;
+	} else {
+	    this->v += obj::get< obj::Atom<T> >(o).v;
+	}
     }
 };
 
 template <typename T>
-void sum_atom(const obj::Object* in, obj::Object*& out) {
+void sum_mul_atom(const obj::Object* in, obj::Object*& out) {
     obj::Atom<T>& x = obj::get< obj::Atom<T> >(in);
     obj::Atom<T>& y = obj::get< obj::Atom<T> >(out);
     y.v = x.v;
 }
 
-template <typename T>
-void sum_arr(const obj::Object* in, obj::Object*& out) {
+template <bool MUL, typename T>
+void sum_mul_arr(const obj::Object* in, obj::Object*& out) {
     obj::ArrayAtom<T>& x = obj::get< obj::ArrayAtom<T> >(in);
     obj::Atom<T>& y = obj::get< obj::Atom<T> >(out);
 
-    y.v = 0;
+    y.v = (MUL ? 1 : 0);
 
     for (T i : x.v) {
-        y.v += i;
+	if (MUL) {
+	    y.v *= i;
+	} else {
+	    y.v += i;
+	}
     }
 }    
 
-template <typename T>
-void sum_seq(const obj::Object* in, obj::Object*& out) {
+template <bool MUL, typename T>
+void sum_mul_seq(const obj::Object* in, obj::Object*& out) {
     obj::Atom<T>& y = obj::get< obj::Atom<T> >(out);
 
-    y.v = 0;
+    y.v = (MUL ? 1 : 0);
 
     while (1) {
         obj::Object* ret = ((obj::Object*)in)->next();
@@ -47,11 +55,16 @@ void sum_seq(const obj::Object* in, obj::Object*& out) {
 
         obj::Atom<T>& x = obj::get< obj::Atom<T> >(ret);
 
-        y.v += x.v;
+	if (MUL) {
+	    y.v *= x.v;
+	} else {
+	    y.v += x.v;
+	}
     }
 }    
 
-Functions::func_t sum_checker(const Type& args, Type& ret, obj::Object*& obj) {
+template <bool MUL>
+Functions::func_t sum_mul_checker(const Type& args, Type& ret, obj::Object*& obj) {
 
     if (args.type == Type::ARR) {
 
@@ -64,11 +77,11 @@ Functions::func_t sum_checker(const Type& args, Type& ret, obj::Object*& obj) {
 
         switch (t.atom) {
         case Type::INT:
-            return sum_arr<Int>;
+            return sum_mul_arr<MUL, Int>;
         case Type::UINT:
-            return sum_arr<UInt>;
+            return sum_mul_arr<MUL, UInt>;
         case Type::REAL:
-            return sum_arr<Real>;
+            return sum_mul_arr<MUL, Real>;
         default:
             return nullptr;
         }
@@ -84,11 +97,11 @@ Functions::func_t sum_checker(const Type& args, Type& ret, obj::Object*& obj) {
 
         switch (t.atom) {
         case Type::INT:
-            return sum_seq<Int>;
+            return sum_mul_seq<MUL, Int>;
         case Type::UINT:
-            return sum_seq<UInt>;
+            return sum_mul_seq<MUL, UInt>;
         case Type::REAL:
-            return sum_seq<Real>;
+            return sum_mul_seq<MUL, Real>;
         default:
             return nullptr;
         }
@@ -99,14 +112,14 @@ Functions::func_t sum_checker(const Type& args, Type& ret, obj::Object*& obj) {
 
         switch (args.atom) {
         case Type::INT:
-            obj = new AtomSum<Int>;
-            return sum_atom<Int>;
+            obj = new AtomSumMul<MUL, Int>;
+            return sum_mul_atom<Int>;
         case Type::UINT:
-            obj = new AtomSum<UInt>;
-            return sum_atom<UInt>;
+            obj = new AtomSumMul<MUL, UInt>;
+            return sum_mul_atom<UInt>;
         case Type::REAL:
-            obj = new AtomSum<Real>;
-            return sum_atom<Real>;
+            obj = new AtomSumMul<MUL, Real>;
+            return sum_mul_atom<Real>;
         default:
             return nullptr;
         }
@@ -115,9 +128,62 @@ Functions::func_t sum_checker(const Type& args, Type& ret, obj::Object*& obj) {
     return nullptr;
 }
 
+
+template <bool MUL, typename OBJ>
+void add_mul_func(const obj::Object* in, obj::Object*& out) {
+
+    OBJ& r = obj::get<OBJ>(out);
+    obj::Tuple& args = obj::get<obj::Tuple>(in);
+
+    r.v = (MUL ? 1 : 0);
+
+    for (obj::Object* arg : args.v) {
+	if (MUL) {
+	    r.v *= obj::get<OBJ>(arg).v;
+	} else {
+	    r.v += obj::get<OBJ>(arg).v;
+	}
+    }
+}
+
+template <bool MUL>
+Functions::func_t add_mul_checker(const Type& args, Type& ret, obj::Object*& obj) {
+
+    if (args.type != Type::TUP || !args.tuple || args.tuple->size() < 1)
+        return nullptr;
+
+    for (const auto& t : *(args.tuple)) {
+
+        if (check_numeric(t)) {
+
+	    ret = t;
+
+	    switch (args.atom) {
+	    case Type::INT:
+		return add_mul_func<MUL, obj::Int>;
+	    case Type::UINT:
+		return add_mul_func<MUL, obj::UInt>;
+	    case Type::REAL:
+		return add_mul_func<MUL, obj::Real>;
+	    default:
+		return nullptr;
+	    }
+
+	} else {
+            return nullptr;
+	}
+    }
+
+    return nullptr;
+}
+
+
 void register_sum(Functions& funcs) {
 
-    funcs.add_poly("sum", sum_checker);
+    funcs.add_poly("sum", sum_mul_checker<false>);
+    funcs.add_poly("product", sum_mul_checker<true>);
+    funcs.add_poly("add", add_mul_checker<false>);
+    funcs.add_poly("mul", add_mul_checker<true>);
 }
 
 
